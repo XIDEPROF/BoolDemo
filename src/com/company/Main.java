181ba0fb26e95106b7573926d2d4f96b77be4a49
Serge <78102903+MrXides@users.noreply.github.com>
2021-11-06 03:35:43 +0300

    T-01
    t

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+ boolean b;
+ b=false;
+ System.out.println("b = "+ b);
+ b=true;
+ System.out.println("b = "+ b);
+ if(b) System.out.println("!!!");
+ System.out.println("Значение в выражении 10<9 ="+(10>9));
+    }
+}
